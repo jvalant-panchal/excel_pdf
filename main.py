053b560e7ec851beca7f671da36e5e3d24a2282a
Jvalant <elitesolutions.jp@gmail.com>
2023-06-10 16:32:27 +0530

    create minimal pdf for respective excel

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fpdf import FPDF
 import pandas as pd
 import glob
+from pathlib import Path
 
 excel_files = glob.glob("files/*.xlsx")
 print(excel_files)
 
-pdf = FPDF(orientation="P", unit="mm", format="A4" )
+
 for file in excel_files:
     df = pd.read_excel(file)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
 
     for index, row in df.iterrows():
-        print(df)
+        filename = Path(file).stem
+        invoice_no = filename.split("-")[0]
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.add_page()
+        pdf.cell(w=50, h=8, txt= f"Invoice No.{invoice_no}")
+        pdf.output(f"PDFs/{filename}.pdf")
+
 
